bfdd6c315c51c265dcc804de5ec543e62b15f41e
DrexHD <nicknamedrex@gmail.com>
2020-10-10 22:12:28 +0200

    update flight state on command execution

--- src/main/java/me/drex/itsours/command/FlyCommand.java
+++ src/main/java/me/drex/itsours/command/FlyCommand.java
@@ -2,13 +2,12 @@ package me.drex.itsours.command;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import me.drex.itsours.ItsOursMod;
 import me.drex.itsours.user.ClaimPlayer;
 import me.drex.itsours.util.Color;
 import net.kyori.adventure.text.Component;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.Formatting;
 
 public class FlyCommand extends Command {
 
@@ -23,6 +22,7 @@ public class FlyCommand extends Command {
         ServerPlayerEntity player = source.getPlayer();
         ClaimPlayer claimPlayer = (ClaimPlayer) player;
         claimPlayer.toggleFlight();
+        if (ItsOursMod.INSTANCE.getClaimList().get(player.getServerWorld(), player.getBlockPos()) != null) player.abilities.allowFlying = claimPlayer.flightEnabled();
         claimPlayer.sendMessage(Component.text("Claim flight " + (claimPlayer.flightEnabled() ? "enabled" : "disabled")).color(claimPlayer.flightEnabled() ? Color.LIGHT_GREEN : Color.RED));
         return 1;
     }
