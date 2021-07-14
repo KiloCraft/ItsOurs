bdce3e70016040bb734ea893f15b5bc4fe5f8905
DrexHD <nicknamedrex@gmail.com>
2021-07-14 14:16:47 +0200

    fix: NPE on first startup

--- src/main/java/me/drex/itsours/claim/list/ClaimList.java
+++ src/main/java/me/drex/itsours/claim/list/ClaimList.java
@@ -26,6 +26,7 @@ public class ClaimList {
     }
 
     public void fromNBT(NbtList tag) {
+        if (tag == null) return;
         tag.forEach(claimTag -> {
             Claim claim = new Claim((NbtCompound) claimTag);
             this.add(claim);
