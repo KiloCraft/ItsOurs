b361b1c21e771ae2ffbbf3c0c4b3372dd489ca58
Drex <nicknamedrex@gmail.com>
2025-04-22 15:22:32 +0200

    Fix mixin

--- src/main/java/me/drex/itsours/mixin/EntityMixin.java
+++ src/main/java/me/drex/itsours/mixin/EntityMixin.java
@@ -53,13 +53,13 @@ public abstract class EntityMixin {
     }
 
     @WrapWithCondition(
-        method = "checkBlockCollision",
+        method = "method_67632",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/block/BlockState;onEntityCollision(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;)V"
+            target = "Lnet/minecraft/block/BlockState;onEntityCollision(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/EntityCollisionHandler;)V"
         )
     )
-    private boolean itsours$onBlockCollision(BlockState blockState, World world, BlockPos pos, Entity entity) {
+    private boolean itsours$onBlockCollision(BlockState blockState, World world, BlockPos pos, Entity entity, EntityCollisionHandler entityCollisionHandler) {
         ServerPlayerEntity playerEntity = null;
         if (entity instanceof ProjectileEntity projectileEntity && (blockState.getBlock() instanceof ButtonBlock || blockState.getBlock() instanceof AbstractPressurePlateBlock)) {
             if (projectileEntity.getOwner() != null && projectileEntity.getOwner() instanceof ServerPlayerEntity) {
